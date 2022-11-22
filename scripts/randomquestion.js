a16a0af71b924b85109ff9d874bee47be3fa616a
Sinan Cekic <typischsinan@gmail.com>
2022-11-22 16:32:28 +0100

    design geändert (Fehler im Zufallsmodus noch vorhanden)

--- scripts/randomquestion.js
+++ scripts/randomquestion.js
@@ -1,7 +1,12 @@
 function zufallsmodus_gefahrenlehre() {
-    var questions = [ '/questions/Gefahrenlehre/1.1.01-001.html', '/questions/Gefahrenlehre/1.1.01-002.html', '/questions/Gefahrenlehre/1.1.01-003.html', '/questions/Gefahrenlehre/1.1.01-004.html']
+    var questions = [ '1.1.01-001.html', '1.1.01-002.html', '1.1.01-003.html', '1.1.01-004.html',
+    '1.1.01-101.html', '1.1.01-102.html', '1.1.01-103.html', '1.1.01-104.html', 
+    '1.1.01-107.html', '1.1.01-108.html', '1.1.01-109.html', '1.1.01-110.html', 
+    '1.1.01-111.html', '1.1.01-112.html', '1.1.01-113.html', '2.1.01-001.html', '2.1.01-003.html', 
+    '2.1.01-004.html']
     window.location.href = questions[Math.floor(Math.random() * questions.length)]
 }
+
 function zufallsmodus_verhalten() {
     var questions = [ 'question1', 'question2', 'question3']
     window.location.href = questions[Math.floor(Math.random() * questions.length)]
